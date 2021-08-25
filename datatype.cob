9c919205e840302814c92c48b31771e505a543f8
Matt Ray <github@mattray.dev>
2021-08-25 17:02:12 +1000

    numbering is hard

--- datatype.cob
+++ datatype.cob
@@ -6,14 +6,14 @@
        LINKAGE SECTION.
        01 ARG1 PIC X(24).
        01 ARG2 PIC X(24).
-       01 Arg4 USAGE COMP-1.
-       01 Arg5 USAGE COMP-2.
-       01 Arg6 BINARY-SHORT SIGNED.
-       PROCEDURE DIVISION USING ARG1 ARG2 Arg4 Arg5 Arg6.
+       01 Arg3 USAGE COMP-1.
+       01 Arg4 USAGE COMP-2.
+       01 Arg5 BINARY-SHORT SIGNED.
+       PROCEDURE DIVISION USING ARG1 ARG2 Arg3 Arg4 Arg5.
        MOVE "Replaced in COBOL" TO ARG2
 
-       add 100.0 to Arg4.
-       subtract 100.0 from Arg5.
-       add 100 to Arg6.
+       add 100.0 to Arg3.
+       subtract 100.0 from Arg4.
+       add 100 to Arg5.
 
        EXIT PROGRAM.
